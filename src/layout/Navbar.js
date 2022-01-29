5f43594351b671cd6c9f29fc5234d9b8e4c3babf
Richard H Dobeck <rdobeck@yahoo.com>
2022-01-28 17:57:11 -0800

    basic navbar shape, added toggler for drop down menu when on small devices, no links yet

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
-    
+  return (
+    <div className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
+        ></div>
+      </div>
+    </div>
+  );
 }
